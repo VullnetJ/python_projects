3fbba0c263b37b6c9c33f1fdd09e08c9fc605246
VullnetJ <46821353+VullnetJ@users.noreply.github.com>
2021-12-15 21:51:37 +0200

    Update python_5.py

--- python_5.py
+++ python_5.py
@@ -107,7 +107,7 @@ for p in range(1, number_of_symbols + 1):
     p_list += random_character
    
 print(p_list)
-# mixing orders with letters, characters and numvers
+# mixing orders with letters, characters and numbers
 random.shuffle(p_list)
 print(p_list)
 
